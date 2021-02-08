306b41288ca43fdb59acfb55c9726f33dfc152ac
Leo <yli97@jmc.com.cn>
2021-02-08 17:20:54 +0800

    添加mdc can驱动

---
+++ modules/drivers/canbus/can_client/mdc/mdc_can_client.cc
@@ -0,0 +1,182 @@
+#include "modules/drivers/canbus/can_client/mdc/mdc_can_client.h"
+
+#include <cassert>
+#include <cstdlib>
+#include <cstring>
+#include <exception>
+#include <iomanip>
+#include <iostream>
+#include <stdint.h>
+#include <string>
+#include <vector>
+
+namespace jmc_auto {
+namespace drivers {
+namespace canbus {
+namespace can {
+
+using jmc_auto::common::ErrorCode;
+
+bool MdcCanClient::Init(const CANCardParameter &param) {
+    if (!parameter.has_channel_id()) {
+        AERROR << "Init CAN failed: parameter does not have channel id. The "
+                  "parameter is "
+               << parameter.DebugString();
+        return false;
+    }
+    if (parameter.channel_id() < 0 || parameter.channel_id() > CAN_NUM) {
+        AERROR << "Init CAN failed: parameter have wrong channel id. The "
+                  "parameter is "
+               << parameter.DebugString();
+        return false;
+    }
+
+    m_channelId = parameter.channel_id();
+    m_instance = m_channelId + 1;
+
+    return true;
+}
+
+ErrorCode MdcCanClient::Start() {
+    if (is_started_) {
+        return ErrorCode::OK;
+    }
+
+    // 提供服务
+    m_skeleton[channelId] = std::make_unique<CanTxSkeleton>(
+        ara::com::InstanceIdentifier(m_instance),
+        ara::com::MethodCallProcessingMode::kPoll);
+    m_skeleton[channelId]->OfferService();
+
+    // 注册服务发现的回调函数，，当发现服务的时候，会回调该函数
+    CanRxProxy::StartFindService(
+        [this](ara::com::ServiceHandleContainer<CanRxProxy::HandleType> handles,
+               ara::com::FindServiceHandle handler) {
+            McuApInterface::ServiceAvailabilityCallback(std::move(handles),
+                                                        handler);
+        },
+        m_instance);
+    is_started_ = true;
+    return ErrorCode::OK;
+}
+
+void MdcCanClient::ServiceAvailabilityCallback(
+    ara::com::ServiceHandleContainer<CanRxProxy::HandleType> handles,
+    ara::com::FindServiceHandle handler) {
+    if (handles.size() > 0) {
+        for (unsigned int i = 0; i < handles.size(); i++) {
+            int instanceId = static_cast<uint16_t>(handles[i].GetInstanceId());
+            int channelID = m_channelId;
+            if (instanceId != m_instance) {
+                continue;
+            }
+            if (m_proxy[channelID] == nullptr) {
+                // 注册接收MCU上传CAN帧的回调函数
+                m_proxy[channelID] = std::make_unique<CanRxProxy>(handles[i]);
+                m_proxy[channelID]->CanDataRxEvent.Subscribe(
+                    ara::com::EventCacheUpdatePolicy::kNewestN, BUFFER_DEPTH);
+                m_proxy[channelID]->CanDataRxEvent.SetReceiveHandler(
+                    [this, channelID]() {
+                        McuApInterface::CanDataEventCallback();
+                    });
+
+                // 开启method发送线程
+                m_canMethodThread[channelID] = std::make_unique<std::thread>(
+                    &McuApInterface::CanMehodSend, this, channelID);
+            }
+        }
+    }
+}
+
+void MdcCanClient::CanDataEventCallback() {
+    if (channelID < 0 || channelID > CAN_NUM) {
+        return;
+    }
+
+    if (m_proxy[channelID] == nullptr) {
+        return;
+    }
+
+    // 加锁防止重入
+    std::unique_lock<std::mutex> lockread(m_canReadMutex);
+    // 接收CAN帧
+    m_proxy[channelID]->CanDataRxEvent.Update();
+    const auto &canMsgSamples =
+        m_proxy[channelID]->CanDataRxEvent.GetCachedSamples();
+    canRevDataParam = canMsgSamples.get();
+    /*for (const auto &canData : canMsgSamples) {
+    for (unsigned int i = 0; i < canData->elementList.size(); i++) {
+        printf("canId: %x, canDLC: %u\n", canData.elementList[i].canId,
+    canData.elementList[i].validLen); for (unsigned int j = 0; j < CAN_VALIDLEN;
+    j++) { printf("%x ", canData.elementList[i].data[j]);
+        }
+        printf("\n");
+    }
+    }*/
+    // 解锁
+    lockread.unlock();
+    m_proxy[channelID]->CanDataRxEvent.Cleanup();
+}
+
+void MdcCanClient::Stop() {}
+
+ErrorCode MdcCanClient::Send(const std::vector<CanFrame> &frames,
+                             int32_t *const frame_num) {
+
+    if (m_proxy[m_channelId] == nullptr) {
+        return;
+    }
+
+    CanBusDataParam canSendDataParm;
+    canSendDataParm.elementList.resize(frame_num);
+    for (int i = 0; i < frame_num; i++) {
+        struct Element canRawdata;
+
+        canRawdata.timeStamp.second = frames.timestamp.tv_sec;
+        canRawdata.timeStamp.nsecond = frames.timestamp.tv_usec;
+
+        // 下发的CAN帧，需要在canbus_config.json中配置，比如CanId、DataLength
+        // 可参照canbus_config_ars408.json中 ChannelId 1 进行配置
+        canRawdata.canId = frames[i].id;
+        canRawdata.validLen = frames[i].len;
+        canSendDataParm.elementList[i].data.resize(DATA_SIZE);
+
+        for (int j = 0; j < CAN_VALIDLEN; i++) {
+            canRawdata.data.push_back(frames[i].data[j]);
+        }
+        canSendDataParm.elementList[i] = canRawdata;
+    }
+    canSendDataParm.seq = 1;
+
+    // Method发送
+    auto handle = m_proxy[m_channelId]->CanDataSetMethod(canSendDataParm);
+
+    return ErrorCode::OK;
+}
+
+ErrorCode MdcCanClient::Receive(std::vector<CanFrame> *const frames,
+                                int32_t *const frame_num) {
+    if (frame_num == nullptr || frames == nullptr) {
+        AERROR << "frames or frame_num pointer is null";
+        return ErrorCode::CAN_CLIENT_ERROR_BASE;
+    }
+    frames->resize(*frame_num);
+    for (size_t i = 0; i < frames->size(); ++i) {
+        for (int j = 0; j < CAN_VALIDLEN; ++j) {
+            (*frames)[i].data[j] = (*canRevDataParm).elementList[i].data[j];
+        }
+        (*frames)[i].id = (*canRevDataParm).elementList[i].canId;
+        (*frames)[i].len = (*canRevDataParm).elementList[i].validLen;
+        ADEBUG << (*frames)[i].CanFrameString() << "frame_num[" << i << "]";
+    }
+    return ErrorCode::OK;
+}
+
+std::string MdcCanClient::GetErrorString(const int32_t /*status*/) {
+    return "";
+}
+
+} // namespace can
+} // namespace canbus
+} // namespace drivers
+} // namespace jmc_auto
---
+++ modules/drivers/canbus/can_client/mdc/mdc_can_client.h
@@ -0,0 +1,99 @@
+#ifndef MODULES_DRIVERS_CANBUS_CAN_CLIENT_MDC_MDC_CAN_CLIENT_H_
+#define MODULES_DRIVERS_CANBUS_CAN_CLIENT_MDC_MDC_CAN_CLIENT_H_
+
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "modules/common/proto/error_code.pb.h"
+#include "modules/drivers/canbus/can_client/can_client.h"
+
+#include "mdc/sensor/canrxserviceinterface_common.h"
+#include "mdc/sensor/canrxserviceinterface_proxy.h"
+#include "mdc/sensor/cantxserviceinterface_common.h"
+#include "mdc/sensor/cantxserviceinterface_skeleton.h"
+
+#include "impl_type_cansetdataresult.h"
+#include "impl_type_canbusdataparam.h
+
+/**
+ * @namespace jmc_auto::drivers::canbus::can
+ * @brief jmc_auto::drivers::canbus::can
+ */
+namespace jmc_auto {
+namespace drivers {
+namespace canbus {
+namespace can {
+
+class MdcCanClient : public CanClient {
+  public:
+    using CanRxProxy = mdc::sensor::proxy::CanRxServiceInterfaceProxy;
+    using CanTxSkeleton = mdc::sensor::skeleton::CanTxServiceInterfaceSkeleton;
+    /// Interval of sleeping
+
+    const unsigned int CAN_NUM = 12;
+    const int CAN_VALIDLEN = 8;
+
+    bool Init(const CANCardParameter &param) override;
+
+    virtual ~MdcCanClient() = default;
+
+    jmc_auto::common::ErrorCode Start() override;
+
+    void Stop() override;
+
+    /**
+     * @brief Send messages
+     * @param frames The messages to send.
+     * @param frame_num The amount of messages to send.
+     * @return The status of the sending action which is defined by
+     *         jmc_auto::common::ErrorCode.
+     */
+    jmc_auto::common::ErrorCode Send(const std::vector<CanFrame> &frames,
+                                     int32_t *const frame_num) override;
+
+    /**
+     * @brief Receive messages
+     * @param frames The messages to receive.
+     * @param frame_num The amount of messages to receive.
+     * @return The status of the receiving action which is defined by
+     *         jmc_auto::common::ErrorCode.
+     */
+    jmc_auto::common::ErrorCode Receive(std::vector<CanFrame> *frames,
+                                        int32_t *const frame_num) override;
+
+    /**
+     * @brief Get the error string.
+     * @param status The status to get the error string.
+     */
+    std::string GetErrorString(const int32_t status) override;
+
+  private:
+    CanBusDataParam *canRevDataParm;
+    std::stringstream frame_info_;
+
+    // canbus_config.json中的ChannelId
+    int m_channelId;
+    // instance ID
+    int m_instance;
+
+    std::mutex m_canReadMutex;
+    std::unique_ptr<CanRxProxy> m_proxy[CAN_NUM];
+    std::mutex m_canSendMutex;
+    std::unique_ptr<CanTxSkeleton> m_skeleton[CAN_NUM];
+    std::unique_ptr<std::thread> m_canMethodThread[CAN_NUM];
+
+    // 服务发现回调
+    void ServiceAvailabilityCallback(
+        ara::com::ServiceHandleContainer<CanRxProxy::HandleType> handles,
+        ara::com::FindServiceHandle handler);
+    // 接收数据回调
+    void CanDataEventCallback(unsigned char channelID);
+};
+
+} // namespace can
+} // namespace canbus
+} // namespace drivers
+} // namespace jmc_auto
+
+#endif // MODULES_DRIVERS_CANBUS_CAN_CLIENT_MDC_MDC_CAN_CLIENT_H_
